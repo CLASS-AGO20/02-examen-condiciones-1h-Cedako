891b55a2791f6964010f01a12bd1a328dc1f4804
Joshua <mramos29@ucol.mx>
2020-11-26 15:12:46 -0600

    costoRenta

--- src/main.js
+++ src/main.js
@@ -8,6 +8,28 @@ export default class App {
         }
     }
 
+    costoRenta(kilometros) {
+        if (kilometros<50) {
+            return kilometros*3;
+        }
+        else if (kilometros>=50 && kilometros<100) {
+            return kilometros*5;
+        }
+        else if (kilometros>=100 && kilometros<200) {
+            return kilometros*6;
+        }
+        else if (kilometros>=200) {
+            return kilometros*6.5;
+        }
+    }
+
 }
 
 let app = new App();
+
+console.log(app.convertir(1999));
+console.log(app.convertir(2000));
+console.log(app.costoRenta(50));
+console.log(app.costoRenta(60));
+console.log(app.costoRenta(101));
+console.log(app.costoRenta(300));
