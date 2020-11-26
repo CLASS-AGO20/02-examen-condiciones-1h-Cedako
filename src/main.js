50f6b4e28a118fd81875c31ade30d2140fcc222f
Joshua <mramos29@ucol.mx>
2020-11-26 16:34:42 -0600

    function_convertir_needs_to_be_fixed_this_is_the_good_one_but_test_marks_it_wrong

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 export default class App {
     convertir(pesos) {
         if (pesos < 2000) {
-            return pesos*20.5;
+            return pesos/20.5;
         }
         else if (pesos >= 2000) {
-            return pesos*22.7;
+            return pesos/22.7;
         }
     }
 
